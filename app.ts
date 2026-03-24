77acc049d803adeea453e33e23ca694d5771d393
Pascal Thuma <pascal.thuma@gmail.com>
2026-03-25 00:00:10 +0100

    fix colors 1

--- app.ts
+++ app.ts
@@ -1,11 +1,25 @@
 import app from "ags/gtk4/app"
-import style from "./style.scss"
+import { exec } from "ags/process"
+import GLib from "gi://GLib?version=2.0"
 import Bar from "./widget/Bar"
 import PowerMenu from "./widget/PowerMenu"
 import { restoreTheme } from "./theme"
 
+const HOME = GLib.getenv("HOME") || ""
+const colorsPath = `${HOME}/.cache/wal/colors.scss`
+
+if (!GLib.file_test(colorsPath, GLib.FileTest.EXISTS)) {
+  console.log("No pywal colors found, generating default from pywal.png...")
+  const agsDir = `${HOME}/.config/ags`
+  exec(`wal -q -n -i ${agsDir}/pywal.png`)
+}
+
+const scss = `${HOME}/.config/ags/style.scss`
+const css = `/tmp/ags-compiled-style.css`
+exec(`sass ${scss} ${css}`)
+
 app.start({
-  css: style,
+  css: css,
   main() {
     restoreTheme()
     app.get_monitors().map(Bar)
